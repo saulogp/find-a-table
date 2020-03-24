e5b0d5c013e98e2a7059497484908c682e1aa553
saulogp <sauloproetti@gmail.com>
2020-03-24 13:07:06 -0300

    criação splash screen

---
+++ lib/pages/splash.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 90),
+              child: Image.asset(
+                "assets/images/logo-nome.png",
+                height: 250,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 200),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "FROM",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black45,
+                    ),
+                  ),
+                  Text(
+                    "W4S",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF002B32),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
